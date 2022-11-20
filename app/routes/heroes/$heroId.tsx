24a1f9b9a1a85546e542a29134860db3ebc73215
wakababa <altgblbl@gmail.com>
2022-11-20 21:12:01 +0300

    ui fix

--- app/routes/heroes/$heroId.tsx
+++ app/routes/heroes/$heroId.tsx
@@ -18,8 +18,8 @@ const ChangeHero = () => {
 
   return (
     <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-      <a style={{ fontSize: 25, textDecoration: "none" }} href={`${Number(heroId) - 1}`}>Back</a>
-      <a style={{ fontSize: 25, textDecoration: "none" }} href={`${Number(heroId) + 1}`}>Next</a>
+      <a style={{ fontSize: 25, textDecoration: "none" ,color:"white"}} href={`${Number(heroId) - 1}`}>Back</a>
+      <a style={{ fontSize: 25, textDecoration: "none" ,color:"white"}} href={`${Number(heroId) + 1}`}>Next</a>
     </div>
   );
 };
@@ -27,12 +27,13 @@ const ChangeHero = () => {
 export default function Hero() {
 
   const { hero } = useLoaderData();
-  const {id, name_loc, bio_loc, abilities } = hero;
-  console.log("abilities",abilities)
+  const {id, name_loc, bio_loc,npe_desc_loc, abilities } = hero;
+  console.log("hero",hero)
   return (
-    <div key={id} style={{ padding: 10 }}>
+          <div key={id} style={{ padding: 10,backgroundImage:'url("https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react//backgrounds/greyfade.jpg")',height:"99vh",color:"white" }}>
       <ChangeHero />
       <h1>{name_loc}</h1>
+      <h5 style={{color:"#a5e0f3",fontSize:18}}>{npe_desc_loc}</h5>
         <p style={{ width: "100%", minWidth: 600, maxWidth: "70%", fontWeight: 200, fontSize: 24 }}>{bio_loc}</p>
         {abilities.map((ability: { name: String; id:Number })=><img key={abilities.id} src={`https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/abilities/${ability.name}.png`} />)}
     </div>
